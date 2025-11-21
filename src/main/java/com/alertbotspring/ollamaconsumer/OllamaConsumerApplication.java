7daf62ec6d2302302f68bc6d5c0f0d956ed564df
Laia Mestres <laia.mestres.st@emeal.nttdata.com>
2025-11-21 15:15:04 +0100

    first commit

---
+++ src/main/java/com/alertbotspring/ollamaconsumer/OllamaConsumerApplication.java
@@ -0,0 +1,11 @@
+package com.alertbotspring.ollamaconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OllamaConsumerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(OllamaConsumerApplication.class, args);
+    }
+}
---
+++ src/main/java/com/alertbotspring/ollamaconsumer/service/WhatsAppConsumer.java
@@ -0,0 +1,18 @@
+package com.alertbotspring.ollamaconsumer.service;
+
+import com.alertbotspring.avro.WhatsAppMessage;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class WhatsAppConsumer {
+
+    private final String TOPIC = "whatsapp-in";
+    private final String GROUP_ID = "whatsapp-in-group";
+
+    @KafkaListener(topics = TOPIC, groupId = GROUP_ID)
+    public void consumeWhatsAppIn(WhatsAppMessage whatsAppMessage) {
+        System.out.println(whatsAppMessage.getText());
+    }
+
+}
