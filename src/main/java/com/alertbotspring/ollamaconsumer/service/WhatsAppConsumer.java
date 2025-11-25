b9caddfd9a2022db39fd4693570395d5cff77688
Laia Mestres <laia.mestres.st@emeal.nttdata.com>
2025-11-25 13:29:20 +0100

    update de logs

--- src/main/java/com/alertbotspring/ollamaconsumer/service/WhatsAppConsumer.java
+++ src/main/java/com/alertbotspring/ollamaconsumer/service/WhatsAppConsumer.java
@@ -1,5 +1,8 @@
 package com.alertbotspring.ollamaconsumer.service;
 
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import com.alertbot.avro.WhatsAppMessage;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
@@ -9,10 +12,12 @@ public class WhatsAppConsumer {
 
     private final String TOPIC = "whatsapp-in";
     private final String GROUP_ID = "whatsapp-in-group";
+    private static final Logger log = LoggerFactory.getLogger(WhatsAppConsumer.class);
 
     @KafkaListener(topics = TOPIC, groupId = GROUP_ID)
     public void consumeWhatsAppIn(WhatsAppMessage whatsAppMessage) {
-        System.out.println(whatsAppMessage.getText());
+
+        log.info("✅ Mensaje Avro enviado al topic " + TOPIC + "{}", whatsAppMessage.getText());
     }
 
 }
