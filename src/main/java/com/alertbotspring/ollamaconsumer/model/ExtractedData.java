290d2d3c0d8444c97d215d11584d1830fb6af110
Laia Mestres <laia.mestres.st@emeal.nttdata.com>
2025-12-03 11:16:11 +0100

    nuevo parámetro

--- src/main/java/com/alertbotspring/ollamaconsumer/model/ExtractedData.java
+++ src/main/java/com/alertbotspring/ollamaconsumer/model/ExtractedData.java
@@ -9,15 +9,17 @@ public class ExtractedData {
     private String level;
     private String price_max;
     private String duration_max;
+    private String lang;
 
     public ExtractedData() {
     }
 
-    public ExtractedData(String product, String level, String price_max, String duration_max) {
+    public ExtractedData(String product, String level, String price_max, String duration_max, String lang) {
         this.product = product;
         this.level = level;
         this.price_max = price_max;
         this.duration_max = duration_max;
+        this.lang = lang;
     }
 }
 
