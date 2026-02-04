91f83dff53f5b2256764050fb8d95745080f24cb
Laia Mestres <laia.mestres.st@emeal.nttdata.com>
2026-02-04 13:43:44 +0100

    primary bean

--- src/main/java/com/alertbotspring/ollamaconsumer/config/GroqConfig.java
+++ src/main/java/com/alertbotspring/ollamaconsumer/config/GroqConfig.java
@@ -6,11 +6,13 @@ import org.springframework.ai.openai.api.OpenAiApi;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 
 @Configuration
 public class GroqConfig {
 
     @Bean
+    @Primary
     public ChatClient.Builder chatClientBuilder (
             @Value("${SPRING_AI_OPENAI_API_KEY}") String apiKey,
             @Value("${SPRING_AI_OPENAI_BASE_URL") String baseUrl) {
