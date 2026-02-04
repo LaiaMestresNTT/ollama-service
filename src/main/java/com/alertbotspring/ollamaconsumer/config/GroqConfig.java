be557b3fdbffc63c664cbb44f0eb59aefe7fe4d7
Laia Mestres <laia.mestres.st@emeal.nttdata.com>
2026-02-04 16:04:28 +0100

    sin config

--- src/main/java/com/alertbotspring/ollamaconsumer/config/GroqConfig.java
+++ src/main/java/com/alertbotspring/ollamaconsumer/config/GroqConfig.java
@@ -2,6 +2,7 @@ package com.alertbotspring.ollamaconsumer.config;
 
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.openai.OpenAiChatModel;
+import org.springframework.ai.openai.OpenAiChatOptions;
 import org.springframework.ai.openai.api.OpenAiApi;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -11,16 +12,22 @@ import org.springframework.context.annotation.Primary;
 @Configuration
 public class GroqConfig {
 
-    @Bean
-    @Primary
+    /*@Bean
     public ChatClient.Builder customChatClientBuilder(
             @Value("${SPRING_AI_OPENAI_API_KEY}") String apiKey,
-            @Value("${SPRING_AI_OPENAI_BASE_URL}") String baseUrl) {
+            @Value("${SPRING_AI_OPENAI_BASE_URL}") String baseUrl,
+            @Value("${spring.ai.openai.chat.options.model}") String modelName) {
 
         var openAiApi = new OpenAiApi(baseUrl, apiKey);
-        var chatModel = new OpenAiChatModel(openAiApi);
+
+        var options = OpenAiChatOptions.builder()
+                .withModel(modelName)
+                .build();
+
+        // 3. Crear el modelo con la API y las opciones
+        var chatModel = new OpenAiChatModel(openAiApi, options);
 
         return ChatClient.builder(chatModel);
-    }
+    }*/
 
 }
